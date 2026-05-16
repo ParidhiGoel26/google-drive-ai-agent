c91631f74e04d506410ce4660ec395e10685b77c
Paridhi Goel <paridhigoyal2606@gmail.com>
2026-05-16 16:38:01 +0530

    done

--- backend/app/services/drive_service.py
+++ backend/app/services/drive_service.py
@@ -13,49 +13,14 @@ class GoogleDriveService:
 
     def search_files(self, folder_id, query):
 
-        query = query.lower()
-
-        drive_query = (
-            f"'{folder_id}' in parents and trashed=false"
+        final_query = (
+            f"'{folder_id}' in parents "
+            f"and trashed=false "
+            f"and {query}"
         )
 
-        # PDFs
-        if "pdf" in query:
-
-            drive_query += (
-                " and mimeType='application/pdf'"
-            )
-
-        # Videos
-        elif "video" in query:
-
-            drive_query += (
-                " and mimeType contains 'video/'"
-            )
-
-        # Images
-        elif "image" in query:
-
-            drive_query += (
-                " and mimeType contains 'image/'"
-            )
-
-        # Excel
-        elif "excel" in query or "sheet" in query:
-
-            drive_query += (
-                " and mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'"
-            )
-
-        # Folders
-        elif "folder" in query:
-
-            drive_query += (
-                " and mimeType='application/vnd.google-apps.folder'"
-            )
-
         results = self.service.files().list(
-            q=drive_query,
+            q=final_query,
             pageSize=20,
             fields="files(id,name,mimeType,modifiedTime,webViewLink)"
         ).execute()
